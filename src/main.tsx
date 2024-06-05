87da7fa7c7bbacb5217a7092aea247ae00a055be
uros-simeunovic <dixypixy007@gmail.com>
2024-06-05 22:57:36 +0200

    AnimatePresence fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,13 @@ import "./index.css";
 import "./main.css";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AnimatePresence mode="wait" initial={false}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </AnimatePresence>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
