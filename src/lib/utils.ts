43da51299142219a5417707302049eabd20382f5
uros-simeunovic <dixypixy007@gmail.com>
2024-06-20 00:55:21 +0200

    Refactor: Clean up utils file by removing unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -30,23 +30,3 @@ export const useScreenSize = () => {
 
   return screenSize;
 };
-
-export const getGoogleUrl = () => {
-  const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
-
-  const options = {
-    redirect_uri: import.meta.env.VITE_GOOGLE_OAUTH_REDIRECT_URL as string,
-    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID as string,
-    access_type: "offline",
-    response_type: "code",
-    prompt: "consent",
-    scope: [
-      "https://www.googleapis.com/auth/userinfo.profile",
-      "https://www.googleapis.com/auth/userinfo.email",
-    ].join(" "),
-  };
-
-  const qs = new URLSearchParams(options);
-
-  return `${rootUrl}?${qs.toString()}`;
-};
