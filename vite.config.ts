823ffdd4263c743aca0e715cd30305e3c96a3ab2
uros-simeunovic <dixypixy007@gmail.com>
2024-09-12 14:41:38 +0200

    image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
